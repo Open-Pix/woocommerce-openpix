bb002f624b0f6445f5950f6f4ba4b1a57eaa90b8
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2021-04-09 15:13:34 -0300

    feat(instructions): improve instructoins

--- templates/payment-instructions.php
+++ templates/payment-instructions.php
@@ -5,30 +5,32 @@ if (!defined('ABSPATH')) {
 } ?>
 
 <div style="margin-bottom: 40px">
-    <p style="width: 100%; text-align: center">
-        Efetue o pagamento PIX usando o <strong>QRCode</strong> ou
-        usando <strong>PIX copia e cola</strong>, se preferir:
+    <p class="openpix-text-align-center">
+        Efetue o pagamento Pix usando o <string>QRCode</string> ou usando
+        <strong>Pix copia e cola</strong>, se preferir:
     </p>
-    <div>
-        <div>
+
+    <div class="openpix-container">
+        <div class="openpix-qrcode-container">
             <img
-                src="<?php echo $qrCodeImage; ?>"
-                title="Código de barras do PIX deste pedido."
-            /><br />
+                    class="openpix-qrcode-image"
+                    title="QRCode Pix deste pedido."
+                        src="<?php echo $qrCodeImage; ?>"
+            />
         </div>
-        <div>
+        <div class="openpix-instructions">
             <ul>
                 <li>
-              <span
-              >Abra o app do seu banco ou instituição financeira e
-                <strong>entre no ambiente Pix</strong>.</span
-              >
+              <span>
+                Abra o app do seu banco ou instituição financeira e
+                <strong>entre no ambiente Pix</strong>.
+              </span>
                 </li>
                 <li>
-              <span
-              >Escolha a opção <strong>Pagar com QR Code</strong> e escanele o
-                código ao lado.</span
-              >
+              <span>
+                Escolha a opção <strong>Pagar com QR Code</strong> e escaneie o
+                código ao lado.
+              </span>
                 </li>
                 <li>
                     <span>Confirme as informações e finalize o pagamento.</span>
@@ -36,17 +38,19 @@ if (!defined('ABSPATH')) {
             </ul>
         </div>
     </div>
-    <div>
-        <p>Pagar com PIX copia e cola </p>
-        <div class="textarea-container">
-          <textarea id='brCode' readonly="" rows="3">
-              <?php echo $brCode; ?>
-</textarea
-          >
+
+    <div class="openpix-copy-paste-container">
+        <p class="openpix-text-align-center">
+            Pagar com Pix copia e cola
+            <button id="btnCopyEmv" class="openpix-copy-button">Copiar</button>
+        </p>
+        <div class="openpix-textarea-container">
+                      <textarea id="emv" readonly="" rows="3" class="openpix-copy-textarea"><?php echo $brCode; ?></textarea>
         </div>
     </div>
-    <p style="width: 100%; text-align: center; margin-top: 20px">
+    <p class="openpix-text-align-center">
         Após o pagamento, podemos levar alguns segundos para confirmar o seu
         pagamento.<br />Você será avisado assim que isso ocorrer!
     </p>
+</div>
 </div>
