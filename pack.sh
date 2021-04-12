ecbd0f7961a831651624c08bda44275d7e8a2533
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2021-04-12 12:56:04 -0300

    feat(pack): remove backup po pot files

--- pack.sh
+++ pack.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+# remove backup files
+rm **/*.po~ **/*.pot~
 zip -r woocommerce-openpix-1.0.0.zip assets includes/class-wc-openpix-pix.php languages templates woocommerce-openpix.php readme.txt LICENSE.txt
