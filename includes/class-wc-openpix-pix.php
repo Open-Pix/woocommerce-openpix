431c2862089edf65d570e8e6a5d578fba397baac
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2021-04-12 17:48:46 -0300

    feat(global): avoid global functions, prefer static functions instead

--- includes/class-wc-openpix-pix.php
+++ includes/class-wc-openpix-pix.php
@@ -9,35 +9,6 @@ function wc_openpix_assets_url()
     return plugin_dir_url(dirname(__FILE__)) . 'assets/';
 }
 
-// generate UUID uuid_v4
-function uuid_v4()
-{
-    return sprintf(
-        '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-
-        // 32 bits for "time_low"
-        mt_rand(0, 0xffff),
-        mt_rand(0, 0xffff),
-
-        // 16 bits for "time_mid"
-        mt_rand(0, 0xffff),
-
-        // 16 bits for "time_hi_and_version",
-        // four most significant bits holds version number 4
-        mt_rand(0, 0x0fff) | 0x4000,
-
-        // 16 bits, 8 bits for "clk_seq_hi_res",
-        // 8 bits for "clk_seq_low",
-        // two most significant bits holds zero and one for variant DCE1.1
-        mt_rand(0, 0x3fff) | 0x8000,
-
-        // 48 bits for "node"
-        mt_rand(0, 0xffff),
-        mt_rand(0, 0xffff),
-        mt_rand(0, 0xffff)
-    );
-}
-
 class WC_OpenPix_Pix_Gateway extends WC_Payment_Gateway
 {
     public function __construct()
@@ -299,7 +270,7 @@ class WC_OpenPix_Pix_Gateway extends WC_Payment_Gateway
         global $woocommerce;
         $order = wc_get_order($order_id);
 
-        $correlationID = uuid_v4();
+        $correlationID = WC_OpenPix::uuid_v4();
 
         $url = $this->getOpenPixApiUrl() . '/api/openpix/v1/charge';
 
--- woocommerce-openpix.php
+++ woocommerce-openpix.php
@@ -14,15 +14,6 @@ if (!defined('ABSPATH')) {
     exit(); // show nothing if someone open this file directly
 }
 
-function openpix_debug($message)
-{
-    $logger = wc_get_logger();
-    $context = [
-        'source' => 'woocommerce_openpix',
-    ];
-    $logger->debug($message, $context);
-}
-
 /**
  * Check if WooCommerce is active
  **/
@@ -45,7 +36,7 @@ class WC_OpenPix
 {
     const VERSION = '1.0.0';
     // change this to work in development, staging or production
-    //    const OPENPIX_ENV = 'development';
+    //        const OPENPIX_ENV = 'development';
     //    const OPENPIX_ENV = 'staging';
     const OPENPIX_ENV = 'production';
 
@@ -94,6 +85,43 @@ class WC_OpenPix
     public static function get_templates_path()
     {
         return self::get_plugin_path() . 'templates/';
+    }
+
+    public static function debug($message)
+    {
+        $logger = wc_get_logger();
+        $context = [
+            'source' => 'woocommerce_openpix',
+        ];
+        $logger->debug($message, $context);
+    }
+
+    public static function uuid_v4()
+    {
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+
+            // 32 bits for "time_low"
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+
+            // 16 bits for "time_mid"
+            mt_rand(0, 0xffff),
+
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand(0, 0x0fff) | 0x4000,
+
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand(0, 0x3fff) | 0x8000,
+
+            // 48 bits for "node"
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff)
+        );
     }
 
     function get_assets_url()
