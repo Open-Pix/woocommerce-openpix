533470bae5243876d2a7670214be9bd96249949b
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2021-05-27 19:09:39 -0300

    fix(version): fix version

--- woocommerce-openpix.php
+++ woocommerce-openpix.php
@@ -4,7 +4,7 @@
  * Description: WooCommerce OpenPix Payment Gateway
  * Author: OpenPix
  * Author URI: https://openpix.com.br/
- * Version: 1.0.0
+ * Version: 1.2.0
  * Text Domain: woocommerce-openpix
  *
  * @package WooCommerce_OpenPix
