46eb2d57dac08fceac907bf8f3a14d34bd898899
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2022-01-31 10:57:38 -0300

    feat(prod): improve prd release and wp

--- pack.sh
+++ pack.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # remove backup files
 rm **/*.po~ **/*.pot~
+yarn build:prod
 zip -r woocommerce-openpix.zip assets includes/class-wc-openpix-pix.php languages templates woocommerce-openpix.php readme.txt LICENSE.txt
