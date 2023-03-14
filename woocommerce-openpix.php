bc38f6fccefe0948059174bd4c3caf38768ab1d5
Fernando Silva <fernando457829@gmail.com>
2023-03-14 08:57:10 -0300

    build(change-log): v2.1.6 (#850)

--- woocommerce-openpix.php
+++ woocommerce-openpix.php
@@ -4,7 +4,7 @@
  * Description: WooCommerce OpenPix Payment Gateway
  * Author: OpenPix
  * Author URI: https://openpix.com.br/
- * Version: 2.1.5
+ * Version: 2.1.6
  * Text Domain: woocommerce-openpix
  *
  * @package WooCommerce_OpenPix
@@ -34,7 +34,7 @@ function woocommerce_openpix_init()
 
 class WC_OpenPix
 {
-    const VERSION = '2.1.5';
+    const VERSION = '2.1.6';
 
     protected static $instance = null;
 
