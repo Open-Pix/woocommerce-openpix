cc90caa421a61f7cf56cb04b8e1bb3c8bfc3fc24
Eduardo Maciel <mrgamex49@gmail.com>
2023-03-15 15:43:14 -0300

    feat(security): add public key signature validation

--- includes/config/config.php
+++ includes/config/config.php
@@ -1,10 +1,11 @@
-<?php
 
-class OpenPixConfig
+
+<?php class OpenPixConfig
 {
-    public static $OPENPIX_ENV = 'production';
-    public static $OPENPIX_API_URL = 'https://api.openpix.com.br';
-    public static $OPENPIX_PLUGIN_URL = 'https://plugin.openpix.com.br/v1/openpix.js';
+    public static $OPENPIX_ENV = 'development';
+    public static $OPENPIX_API_URL = 'http://localhost:5001';
+    public static $OPENPIX_PLUGIN_URL = 'http://localhost:4444/openpix.js';
+    public static $OPENPIX_PUBLIC_KEY_BASE64 = 'LS0tLSCRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlHZk1BMEdDU3FHU0liM0RRRUJBUVVBQTRHTkFEQ0JpUUtCZ1FEVDdWYStxb3pvT2NYUStjSHJWNk85RVE0TgpnZVhvY1ZwRFBBWkpTZVJsbEVlQVVha051MURqY3FweDFmb1l5aEZxRTM3TkNWYzRtK0hvTC9nN1k3VDMyZVJ4CjhpandxMjdoY0ZjL0RFc01ISWdVU0U4cGdPbi96a3ZadXdNb256MkVjdy85NzZzTlUzNnpKOXhMUE53dURnSysKb2dUb0RQTmNkaWtRdi9STHFRSURBUUFCCi0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=';
 
     public static function getApiUrl()
     {
@@ -23,7 +24,13 @@ class OpenPixConfig
 
     public static function getWebhookUrl()
     {
-        return home_url('/') . 'wc-api/' . 'WC_OpenPix_Pix_Gateway';
+        $webhookUrl = str_replace(
+            'https:',
+            'http:',
+            home_url('/') . 'wc-api/' . 'WC_OpenPix_Pix_Gateway'
+        );
+
+        return $webhookUrl;
     }
 }
 
