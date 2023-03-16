5e705d7df9f26c0795beb52ec80ea81c9083c6be
Fernando Silva <fernandonsilva16@gmail.com>
2023-03-16 10:40:24 -0300

    fix(config): staging

--- includes/config/config-staging.php
+++ includes/config/config-staging.php
@@ -3,8 +3,8 @@
 <?php class OpenPixConfig
 {
     public static $OPENPIX_ENV = 'staging';
-    public static $OPENPIX_API_URL = 'https://api.openpix.dev';
-    public static $OPENPIX_PLUGIN_URL = 'https://plugin.openpix.dev/v1/openpix-dev.js';
+    public static $OPENPIX_API_URL = 'https://api.woovi.dev';
+    public static $OPENPIX_PLUGIN_URL = 'https://plugin.woovi.dev/v1/openpix-dev.js';
     public static $OPENPIX_PUBLIC_KEY_BASE64 = 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlHZk1BMEdDU3FHU0liM0RRRUJBUVVBQTRHTkFEQ0JpUUtCZ1FEVDdWYStxb3pvT2NYUStjSHJWNk85RVE0TgpnZVhvY1ZwRFBBWkpTZVJsbEVlQVVha051MURqY3FweDFmb1l5aEZxRTM3TkNWYzRtK0hvTC9nN1k3VDMyZVJ4CjhpandxMjdoY0ZjL0RFc01ISWdVU0U4cGdPbi96a3ZadXdNb256MkVjdy85NzZzTlUzNnpKOXhMUE53dURnSysKb2dUb0RQTmNkaWtRdi9STHFRSURBUUFCCi0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=';
 
     public static function getApiUrl()
