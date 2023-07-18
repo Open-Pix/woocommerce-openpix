e29bb74e637cc561a8ee1e6c1d4a1013ae578951
Danilo <daniloassis.ti@gmail.com>
2023-07-18 12:35:44 -0300

    chore(config): config

--- includes/config/config-development.php
+++ includes/config/config-development.php
@@ -9,17 +9,17 @@ class OpenPixConfig
     public static $OPENPIX_PLUGIN_URL = 'http://localhost:4444/openpix.js';
     public static $OPENPIX_PUBLIC_KEY_BASE64 = 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlHZk1BMEdDU3FHU0liM0RRRUJBUVVBQTRHTkFEQ0JpUUtCZ1FEVDdWYStxb3pvT2NYUStjSHJWNk85RVE0TgpnZVhvY1ZwRFBBWkpTZVJsbEVlQVVha051MURqY3FweDFmb1l5aEZxRTM3TkNWYzRtK0hvTC9nN1k3VDMyZVJ4CjhpandxMjdoY0ZjL0RFc01ISWdVU0U4cGdPbi96a3ZadXdNb256MkVjdy85NzZzTlUzNnpKOXhMUE53dURnSysKb2dUb0RQTmNkaWtRdi9STHFRSURBUUFCCi0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=';
 
-    public static function getApiUrl()
+    public static function getApiUrl(): string
     {
         return OpenPixConfig::$OPENPIX_API_URL;
     }
 
-    public static function getPluginUrl()
+    public static function getPluginUrl(): string
     {
         return OpenPixConfig::$OPENPIX_PLUGIN_URL;
     }
 
-    public static function getEnv()
+    public static function getEnv(): string
     {
         return OpenPixConfig::$OPENPIX_ENV;
     }
--- includes/config/config.php
+++ includes/config/config.php
@@ -7,17 +7,17 @@ class OpenPixConfig
     public static $OPENPIX_PLUGIN_URL = 'https://plugin.openpix.com.br/v1/openpix.js';
     public static $OPENPIX_PUBLIC_KEY_BASE64 = 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlHZk1BMEdDU3FHU0liM0RRRUJBUVVBQTRHTkFEQ0JpUUtCZ1FDLytOdElranpldnZxRCtJM01NdjNiTFhEdApwdnhCalk0QnNSclNkY2EzcnRBd01jUllZdnhTbmQ3amFnVkxwY3RNaU94UU84aWVVQ0tMU1dIcHNNQWpPL3paCldNS2Jxb0c4TU5waS91M2ZwNnp6MG1jSENPU3FZc1BVVUcxOWJ1VzhiaXM1WloySVpnQk9iV1NwVHZKMGNuajYKSEtCQUE4MkpsbitsR3dTMU13SURBUUFCCi0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=';
     
-    public static function getApiUrl()
+    public static function getApiUrl(): string
     {
         return OpenPixConfig::$OPENPIX_API_URL;
     }
 
-    public static function getPluginUrl()
+    public static function getPluginUrl(): string
     {
         return OpenPixConfig::$OPENPIX_PLUGIN_URL;
     }
 
-    public static function getEnv()
+    public static function getEnv(): string
     {
         return OpenPixConfig::$OPENPIX_ENV;
     }
