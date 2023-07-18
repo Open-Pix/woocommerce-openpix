7e668535c64c915d7c8139bd8db8a93dc6185f2c
Danilo <daniloassis.ti@gmail.com>
2023-07-18 12:29:40 -0300

    chore(config): config

--- includes/config/config.php
+++ includes/config/config.php
@@ -1,14 +1,12 @@
 <?php
 
-// @woovi/do-not-merge
-
 class OpenPixConfig
 {
-    public static $OPENPIX_ENV = 'development';
-    public static $OPENPIX_API_URL = 'http://localhost:5001';
-    public static $OPENPIX_PLUGIN_URL = 'http://localhost:4444/openpix.js';
-    public static $OPENPIX_PUBLIC_KEY_BASE64 = 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlHZk1BMEdDU3FHU0liM0RRRUJBUVVBQTRHTkFEQ0JpUUtCZ1FEVDdWYStxb3pvT2NYUStjSHJWNk85RVE0TgpnZVhvY1ZwRFBBWkpTZVJsbEVlQVVha051MURqY3FweDFmb1l5aEZxRTM3TkNWYzRtK0hvTC9nN1k3VDMyZVJ4CjhpandxMjdoY0ZjL0RFc01ISWdVU0U4cGdPbi96a3ZadXdNb256MkVjdy85NzZzTlUzNnpKOXhMUE53dURnSysKb2dUb0RQTmNkaWtRdi9STHFRSURBUUFCCi0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=';
-
+    public static $OPENPIX_ENV = 'production';
+    public static $OPENPIX_API_URL = 'https://api.openpix.com.br';
+    public static $OPENPIX_PLUGIN_URL = 'https://plugin.openpix.com.br/v1/openpix.js';
+    public static $OPENPIX_PUBLIC_KEY_BASE64 = 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlHZk1BMEdDU3FHU0liM0RRRUJBUVVBQTRHTkFEQ0JpUUtCZ1FDLytOdElranpldnZxRCtJM01NdjNiTFhEdApwdnhCalk0QnNSclNkY2EzcnRBd01jUllZdnhTbmQ3amFnVkxwY3RNaU94UU84aWVVQ0tMU1dIcHNNQWpPL3paCldNS2Jxb0c4TU5waS91M2ZwNnp6MG1jSENPU3FZc1BVVUcxOWJ1VzhiaXM1WloySVpnQk9iV1NwVHZKMGNuajYKSEtCQUE4MkpsbitsR3dTMU13SURBUUFCCi0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=';
+    
     public static function getApiUrl()
     {
         return OpenPixConfig::$OPENPIX_API_URL;
@@ -26,13 +24,7 @@ class OpenPixConfig
 
     public static function getWebhookUrl($gatewayClass = 'WC_OpenPix_Pix_Gateway'): string
     {
-        $webhookUrl = str_replace(
-            'https:',
-            'http:',
-            home_url('/') . 'wc-api/' . $gatewayClass
-        );
-
-        return $webhookUrl;
+        return home_url('/') . 'wc-api/' . $gatewayClass;
     }
 }
 
