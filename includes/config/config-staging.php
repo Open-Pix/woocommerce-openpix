0df4d779fc751593afa72fd14cc3c0d18b2864ff
criskell <cris.moraes.ds@gmail.com>
2023-09-05 13:54:00 -0300

    fix: platform urls

--- includes/config/config-staging.php
+++ includes/config/config-staging.php
@@ -8,7 +8,7 @@ class OpenPixConfig
     public static $OPENPIX_API_URL = 'https://api.woovi.dev';
     public static $OPENPIX_PLUGIN_URL = 'https://plugin.woovi.dev/v1/openpix-dev.js';
     public static $OPENPIX_PUBLIC_KEY_BASE64 = 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlHZk1BMEdDU3FHU0liM0RRRUJBUVVBQTRHTkFEQ0JpUUtCZ1FEVDdWYStxb3pvT2NYUStjSHJWNk85RVE0TgpnZVhvY1ZwRFBBWkpTZVJsbEVlQVVha051MURqY3FweDFmb1l5aEZxRTM3TkNWYzRtK0hvTC9nN1k3VDMyZVJ4CjhpandxMjdoY0ZjL0RFc01ISWdVU0U4cGdPbi96a3ZadXdNb256MkVjdy85NzZzTlUzNnpKOXhMUE53dURnSysKb2dUb0RQTmNkaWtRdi9STHFRSURBUUFCCi0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=';
-    public static $OPENPIX_PLATFORM_URL = 'https://woovi.dev';
+    public static $OPENPIX_PLATFORM_URL = 'https://app.woovi.dev';
 
     public static function getApiUrl(): string
     {
@@ -25,8 +25,9 @@ class OpenPixConfig
         return OpenPixConfig::$OPENPIX_ENV;
     }
 
-    public static function getWebhookUrl($gatewayClass = 'WC_OpenPix_Pix_Gateway'): string
-    {
+    public static function getWebhookUrl(
+        $gatewayClass = 'WC_OpenPix_Pix_Gateway'
+    ): string {
         return home_url('/') . 'wc-api/' . $gatewayClass;
     }
 
