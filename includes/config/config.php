22d909891e5f2796dc0a295ef803e7924e9024dc
criskell <cris.moraes.ds@gmail.com>
2023-09-10 16:16:45 -0300

    fix(config): use production variables

--- includes/config/config.php
+++ includes/config/config.php
@@ -1,14 +1,12 @@
 <?php
 
-// @woovi/do-not-merge
-
 class OpenPixConfig
 {
-    public static $OPENPIX_ENV = 'development';
-    public static $OPENPIX_API_URL = 'http://host.docker.internal:5001';
-    public static $OPENPIX_PLUGIN_URL = 'http://host.docker.internal:4444/openpix.js';
-    public static $OPENPIX_PUBLIC_KEY_BASE64 = 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlHZk1BMEdDU3FHU0liM0RRRUJBUVVBQTRHTkFEQ0JpUUtCZ1FEVDdWYStxb3pvT2NYUStjSHJWNk85RVE0TgpnZVhvY1ZwRFBBWkpTZVJsbEVlQVVha051MURqY3FweDFmb1l5aEZxRTM3TkNWYzRtK0hvTC9nN1k3VDMyZVJ4CjhpandxMjdoY0ZjL0RFc01ISWdVU0U4cGdPbi96a3ZadXdNb256MkVjdy85NzZzTlUzNnpKOXhMUE53dURnSysKb2dUb0RQTmNkaWtRdi9STHFRSURBUUFCCi0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=';
-    public static $OPENPIX_PLATFORM_URL = 'http://localhost:8103';
+    public static $OPENPIX_ENV = 'production';
+    public static $OPENPIX_API_URL = 'https://api.openpix.com.br';
+    public static $OPENPIX_PLUGIN_URL = 'https://plugin.openpix.com.br/v1/openpix.js';
+    public static $OPENPIX_PUBLIC_KEY_BASE64 = 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlHZk1BMEdDU3FHU0liM0RRRUJBUVVBQTRHTkFEQ0JpUUtCZ1FDLytOdElranpldnZxRCtJM01NdjNiTFhEdApwdnhCalk0QnNSclNkY2EzcnRBd01jUllZdnhTbmQ3amFnVkxwY3RNaU94UU84aWVVQ0tMU1dIcHNNQWpPL3paCldNS2Jxb0c4TU5waS91M2ZwNnp6MG1jSENPU3FZc1BVVUcxOWJ1VzhiaXM1WloySVpnQk9iV1NwVHZKMGNuajYKSEtCQUE4MkpsbitsR3dTMU13SURBUUFCCi0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=';
+    public static $OPENPIX_PLATFORM_URL = 'https://app.openpix.com.br';
 
     public static function getApiUrl(): string
     {
@@ -25,15 +23,10 @@ class OpenPixConfig
         return OpenPixConfig::$OPENPIX_ENV;
     }
 
-    public static function getWebhookUrl($gatewayClass = 'WC_OpenPix_Pix_Gateway'): string
-    {
-        $webhookUrl = str_replace(
-            'https:',
-            'http:',
-            home_url('/') . 'wc-api/' . $gatewayClass
-        );
-
-        return $webhookUrl;
+    public static function getWebhookUrl(
+        $gatewayClass = 'WC_OpenPix_Pix_Gateway'
+    ): string {
+        return home_url('/') . 'wc-api/' . $gatewayClass;
     }
 
     public static function getPlatformUrl(): string
