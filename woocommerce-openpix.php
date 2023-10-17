572c835f47ee47e2cdbcd91159cf90df810b1bed
criskell <96352451+criskell@users.noreply.github.com>
2023-10-17 17:55:20 -0300

    build(change-log): v2.9.1 (#1273)

--- woocommerce-openpix.php
+++ woocommerce-openpix.php
@@ -4,7 +4,7 @@
  * Description: Aceite pagamentos Pix em com atualização em tempo real, checkout transparente e atualização automática de status do pedido.
  * Author: OpenPix
  * Author URI: https://openpix.com.br/
- * Version: 2.9.0
+ * Version: 2.9.1
  * Text Domain: woocommerce-openpix
  * WC tested up to: 7.6.0
  * @package WooCommerce_OpenPix
@@ -34,7 +34,7 @@ function woocommerce_openpix_init()
 
 class WC_OpenPix
 {
-    const VERSION = '2.9.0';
+    const VERSION = '2.9.1';
 
     protected static $instance = null;
 
