892f0ebd607497227258b598bce1d1d637a87aa4
criskell <cris.moraes.ds@gmail.com>
2024-01-26 15:27:09 -0300

    fix(checkout): add checkout blocks compatibility for pix

---
+++ assets/pix-block.js
@@ -0,0 +1,22 @@
+(function () {
+	var data = window.wc.wcSettings.getSetting('woocommerce_openpix_pix_data', {});
+	var label = window.wp.htmlEntities.decodeEntities(data.title)
+		|| window.wp.i18n.__('Checkout Pix', 'woocommerce-openpix');
+	
+	var content = (data) => {
+		return window.wp.htmlEntities.decodeEntities(data.description || '');
+	};
+
+	window.wc.wcBlocksRegistry.registerPaymentMethod({
+		name: 'woocommerce_openpix_pix',
+		label,
+		content: Object(window.wp.element.createElement)(content, null),
+		edit: Object(window.wp.element.createElement)(content, null),
+		canMakePayment: () => true,
+		placeOrderButtonLabel: window.wp.i18n.__('Continue', 'woocommerce-openpix'),
+		ariaLabel: label,
+		supports: {
+			features: data.supports,
+		},
+	});
+})();
